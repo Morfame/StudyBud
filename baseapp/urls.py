15bb11d5360b4c3664dc89a3d2a38f1c77ebccf0
Steve Morris <baramorf.com>
2023-03-14 18:07:34 +0000

    Authentication installed

--- baseapp/urls.py
+++ baseapp/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
+
     path('', views.home, name="home"),
     path('room/<str:pk>/', views.room, name="room"),
 
@@ -9,3 +12,4 @@ urlpatterns = [
     path('update-room/<str:pk>', views.updateRoom, name="update-room"),
     path('delete-room/<str:pk>', views.deleteRoom, name="delete-room"),
 ]
+
